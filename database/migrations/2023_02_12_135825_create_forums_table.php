d2979da416cde45ed3b3a704d4de1280940ffd54
TheGreatKappa <gyorffy.milan@gmail.com>
2023-02-12 15:31:35 +0100

    preparing models

---
+++ database/migrations/2023_02_12_135825_create_forums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forums', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('user')->constrained();
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forums');
+    }
+};
