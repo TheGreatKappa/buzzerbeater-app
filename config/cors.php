2fc335b134de6a40071f4f15ba9b7182d7ff1736
TheGreatKappa <gyorffy.milan@gmail.com>
2023-05-13 20:00:34 +0200

    cors modifications

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => ['*'],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Access-Control-Allow-Origin', '*'],
 
     'exposed_headers' => [],
 
